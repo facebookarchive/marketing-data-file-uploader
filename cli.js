dacb4687d896f0b3bde273bf7aa72fe0748f6f62
Ryan Her <ryanher@fb.com>
2018-11-06 13:26:19 -0500

    Added copyright header to files that missed it

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
 const mdfu = require('./lib/mdfu/MarketingDataFileUploader');
 mdfu.upload();
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
 const mdfu = require('./lib/mdfu/MarketingDataFileUploader');
 
 exports.upload = function() {
